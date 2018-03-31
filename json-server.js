fe425ed14c9ffb01d6f3989557e2265521e30110
汪磊 <w@zce.me>
2018-03-31 21:01:26 +0800

    Update json-server.js

--- json-server.js
+++ json-server.js
@@ -1,4 +1,7 @@
 const path = require('path')
-const { router } = require('json-server')
+const { defaults, router } = require('json-server')
 
-module.exports = () => router(path.join(__dirname, 'database.json'))
+module.exports = () => [
+  defaults({ readOnly: true }),
+  router(path.join(__dirname, 'database.json'))
+]
